1ed2dce597c1b121c5ace483a2e8ca06bfd74922
louiedog7676 <louiedog767@gmail.com>
2026-06-24 00:12:40 -0500

    Add files via upload

---
+++ deobfuscated-2026-04-05T19_44_01.324Z.js
@@ -0,0 +1,248 @@
+var _0x3d3c = [
+  'bwebc',
+  'notification',
+  'Cache-Control',
+  'oimagex',
+  'length',
+  'onLine',
+  'LmaGc',
+  '/web/',
+  'close',
+  'parse',
+  'y99.in',
+  'headers',
+  'skipWaiting',
+  'fIqvN',
+  'JvOea',
+  'VHhtK',
+  'catch',
+  'Pqcpy',
+  'fetch',
+  'url',
+  'granted',
+  'XbWtr',
+  'djWGT',
+  'message',
+  'Knjgv',
+  'status',
+  '<html><head>\n\t\t\t<title>Offline | Y99</title>\n    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n\n    <style>\n\t.heading{\n\t\ttext-align: center;\n\t\tfont-size: 19px;\n\t}\n\tbody {\n    background: #843684;\n}\n    *{font-family:sans-serif;color:white;}\n\t</style>\n\t</head><body>\n\t<div style=\'position:relative;max-width:500px;margin:0 auto\'>\n\t<h1 style="\n    text-align: center;\n">No internet</h1>\n\n<button onclick="location.reload()" \n\tstyle="\n    margin: 0 auto;\n    display: block;\n    padding: 10px 59px;\n    color: purple;\n    border-radius: 31px;\n    border: 3px solid #ab47bc;\n    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);\n">Refresh</button>\n\n    <p class=\'heading\'>Useful tips:</p><br>\n    <ul>\n        <li>Checking the network cables, modem, and router</li>\n        <li>Reconnecting to Wi-Fi or Cellular Data</li>\n\t\t<li>If Airplane mode is on, turn it off</li>\n\t</ul>\n\t\n\t<div class=\'heading\'>Other tips:</div>\n\t\t\t\n\t<ul>\n        <li>Use your neighbour\'s Wi-Fi (with their permission).</li>\n        <li>Other sites work but this one doesn\'t? That\'s weird. Inform the email support: support@y99.in</li>\n    </ul>\n\t</div>\n</body></html>\n',
+  'indexOf',
+  'focus',
+  'xuMbh',
+  'respondWith',
+  'data',
+  'BeWfr',
+  'Content-Type',
+  'addEventListener',
+  'UAZkJ',
+  'object',
+  'notificationclick',
+  'XnZtf',
+  'max-age=0, no-cache, must-revalidate, proxy-revalidate',
+  'jeaoO',
+  'oimage',
+  'ZofbS',
+  'body',
+  'push',
+  'openWindow',
+  'sendNotification',
+  'Notification',
+  'pathname',
+  'showNotification',
+  'log',
+  'pkPtJ',
+  'bePUB',
+  'DjCZq',
+  'waitUntil',
+  'iQuPG',
+  'type',
+  'cUlTT',
+  'then',
+  'BKpGk',
+  'UFtSx',
+  'registration',
+]
+;(function (_0x2ff44c, _0x4e8828) {
+  var _0x25e4be = function (_0x295089) {
+    while (--_0x295089) {
+      _0x2ff44c.push(_0x2ff44c.shift())
+    }
+  }
+  _0x25e4be(++_0x4e8828)
+})(_0x3d3c, 445)
+var _0x4c1d = function (_0x2ff44c, _0x4e8828) {
+  _0x2ff44c = _0x2ff44c - 0
+  var _0x25e4be = _0x3d3c[_0x2ff44c]
+  return _0x25e4be
+}
+let url = 'https://y99.in/web/'
+self[_0x4c1d('0x17')]('install', (_0x350211) => {
+  self[_0x4c1d('0x1')]()
+})
+self[_0x4c1d('0x17')](_0x4c1d('0x7'), function (_0x165d8b) {
+  var _0x2e4da7 = {
+    bwebc: function (_0x537fed, _0x38621a) {
+      return _0x537fed === _0x38621a
+    },
+    UFtSx: _0x4c1d('0x3a'),
+  }
+  _0x2e4da7[_0x4c1d('0x28')] = function (_0x50073c, _0x54a211) {
+    return _0x50073c === _0x54a211
+  }
+  _0x2e4da7[_0x4c1d('0x2e')] = 'text/html'
+  _0x2e4da7[_0x4c1d('0x2a')] = _0x4c1d('0x1c')
+  var _0x12935f = _0x2e4da7
+  let _0xff2bc3 = _0x165d8b.request.url
+  let _0x5295b7 = new URL(_0xff2bc3)
+  if (
+    _0x12935f[_0x4c1d('0x33')](
+      _0x5295b7.pathname[_0x4c1d('0x10')](_0x12935f[_0x4c1d('0x31')]),
+      0
+    ) &&
+    _0x12935f[_0x4c1d('0x28')](
+      _0x5295b7[_0x4c1d('0x25')][_0x4c1d('0x10')]('.'),
+      -1
+    )
+  ) {
+    if (!navigator[_0x4c1d('0x38')]) {
+      var _0x5cd3dd = {}
+      _0x5cd3dd[_0x4c1d('0x16')] = _0x12935f.cUlTT
+      _0x5cd3dd[_0x4c1d('0x35')] = _0x12935f[_0x4c1d('0x2a')]
+      var _0x1e2179 = {}
+      _0x1e2179[_0x4c1d('0xe')] = 200
+      _0x1e2179[_0x4c1d('0x0')] = _0x5cd3dd
+      var _0x2b40ea = new Response(_0x4c1d('0xf'), _0x1e2179)
+      console.log(_0x2b40ea)
+      _0x165d8b[_0x4c1d('0x13')](_0x2b40ea)
+    }
+  }
+})
+self[_0x4c1d('0x17')](_0x4c1d('0x1a'), function (_0x19ba81) {
+  var _0x1cfa66 = { LmaGc: _0x4c1d('0x11') }
+  _0x1cfa66[_0x4c1d('0x1b')] = function (_0x1e3c42, _0x2e980d) {
+    return _0x1e3c42 < _0x2e980d
+  }
+  _0x1cfa66[_0x4c1d('0x15')] = function (_0x2f48af, _0x2f6e75) {
+    return _0x2f48af === _0x2f6e75
+  }
+  _0x1cfa66[_0x4c1d('0x1d')] = function (_0x36f844, _0xbb37a) {
+    return _0x36f844 in _0xbb37a
+  }
+  _0x1cfa66[_0x4c1d('0x30')] = 'window'
+  var _0x41d895 = _0x1cfa66
+  _0x19ba81[_0x4c1d('0x34')][_0x4c1d('0x3b')]()
+  var _0x33ecb0 = {}
+  _0x33ecb0[_0x4c1d('0x2d')] = _0x41d895[_0x4c1d('0x30')]
+  _0x19ba81[_0x4c1d('0x2b')](
+    clients.matchAll(_0x33ecb0)[_0x4c1d('0x2f')]((_0xcb4fb2) => {
+      for (
+        var _0x2cb6d7 = 0;
+        _0x41d895[_0x4c1d('0x1b')](_0x2cb6d7, _0xcb4fb2[_0x4c1d('0x37')]);
+        _0x2cb6d7++
+      ) {
+        var _0x3f4e5c = _0xcb4fb2[_0x2cb6d7]
+        if (
+          _0x41d895.BeWfr(_0x3f4e5c[_0x4c1d('0x8')].indexOf(url), 0) &&
+          _0x41d895.jeaoO(_0x41d895[_0x4c1d('0x39')], _0x3f4e5c)
+        ) {
+          return _0x3f4e5c.focus()
+        }
+      }
+      if (clients[_0x4c1d('0x22')]) {
+        return clients[_0x4c1d('0x22')](url)
+      }
+    })
+  )
+})
+function doSomething() {}
+self[_0x4c1d('0x17')](_0x4c1d('0x21'), function (_0x574d1b) {
+  _0x65a377[_0x4c1d('0x2')] = function (_0x18b3aa, _0x8785f0) {
+    return _0x18b3aa === _0x8785f0
+  }
+  _0x65a377[_0x4c1d('0x1f')] = _0x4c1d('0x9')
+  if (
+    !(
+      self[_0x4c1d('0x24')] &&
+      _0x166054[_0x4c1d('0x2')](
+        self.Notification.permission,
+        _0x166054[_0x4c1d('0x1f')]
+      )
+    )
+  ) {
+    return
+  }
+  if (_0x574d1b[_0x4c1d('0x14')]) {
+    const _0x34759d = _0x574d1b[_0x4c1d('0x14')].text()
+    _0x574d1b[_0x4c1d('0x2b')](
+      _0x166054[_0x4c1d('0x29')](sendNotification, _0x34759d)
+    )
+  }
+})
+const sendNotification = (_0x111e82) => {
+  var _0x3a418b = {}
+  _0x3a418b[_0x4c1d('0x6')] = _0x4c1d('0x3d')
+  _0x3a418b[_0x4c1d('0x4')] = function (_0x276b21, _0x25fb96) {
+    return _0x276b21 !== _0x25fb96
+  }
+  _0x3a418b[_0x4c1d('0x2c')] = _0x4c1d('0x19')
+  _0x3a418b[_0x4c1d('0x12')] = function (_0x4fee44, _0x213804) {
+    return _0x4fee44(_0x213804)
+  }
+  var _0x2a93ce = _0x3a418b
+  var _0x57b86 = _0x2a93ce.Pqcpy
+  var _0x397cde = _0x111e82
+  try {
+    if (_0x2a93ce.VHhtK(typeof _0x397cde, _0x2a93ce.iQuPG)) {
+      _0x397cde = JSON[_0x4c1d('0x3c')](_0x111e82)
+    }
+  } catch (_0x411eff) {}
+  if (_0x2a93ce[_0x4c1d('0x4')](typeof _0x397cde, 'object')) {
+    var _0x19796d = {}
+    _0x19796d[_0x4c1d('0x20')] = _0x111e82
+    return self[_0x4c1d('0x32')].showNotification(_0x57b86, _0x19796d)
+  } else {
+    if (_0x397cde[_0x4c1d('0x1e')]) {
+      _0x2a93ce[_0x4c1d('0x12')](httpPing, _0x397cde[_0x4c1d('0x1e')])
+      if (_0x397cde[_0x4c1d('0x36')]) {
+        return
+      }
+    }
+    url = _0x397cde[_0x4c1d('0x8')]
+    if (_0x397cde[_0x4c1d('0xc')] && !_0x397cde.body) {
+      _0x397cde.body = _0x397cde[_0x4c1d('0xc')]
+    }
+    return self[_0x4c1d('0x32')][_0x4c1d('0x26')](_0x397cde.title, _0x397cde)
+  }
+}
+function httpPing(_0x4c60be) {
+  var _0x4b6bf8 = {}
+  _0x4b6bf8[_0x4c1d('0x3')] = function (_0x5871a0, _0x3e1c57, _0x407944) {
+    return _0x5871a0(_0x3e1c57, _0x407944)
+  }
+  var _0x3f0de2 = _0x4b6bf8
+  var _0x2f25c5 = { method: 'get' }
+  _0x3f0de2[_0x4c1d('0x3')](fetch, _0x4c60be, _0x2f25c5)
+    [_0x4c1d('0x2f')](function (_0x428f5a) {})
+    [_0x4c1d('0x5')](function (_0x12b21d) {})
+}
+self[_0x4c1d('0x17')](_0x4c1d('0xc'), function (_0x2a0215) {
+  _0x1acf6b[_0x4c1d('0xd')] = _0x4c1d('0x23')
+  _0x1acf6b[_0x4c1d('0xa')] = function (_0xd15150, _0x55a3bd) {
+    return _0xd15150(_0x55a3bd)
+  }
+  _0x1acf6b[_0x4c1d('0x18')] = function (_0x2bfc46, _0x12cf8e) {
+    return _0x2bfc46 + _0x12cf8e
+  }
+  if (
+    _0x2a0215[_0x4c1d('0x14')] &&
+    _0x265ce9[_0x4c1d('0xb')](_0x2a0215.data.action, _0x265ce9[_0x4c1d('0xd')])
+  ) {
+    _0x265ce9[_0x4c1d('0xa')](
+      sendNotification,
+      _0x2a0215[_0x4c1d('0x14')][_0x4c1d('0x20')]
+    )
+  }
+  console[_0x4c1d('0x27')](
+    _0x265ce9.UAZkJ('SW Received Message: ', _0x2a0215[_0x4c1d('0x14')])
+  )
+})
